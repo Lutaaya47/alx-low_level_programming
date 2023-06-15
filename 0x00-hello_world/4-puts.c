4db6b753ce0f733e8e866070b6aca50891f89615
Lutaaya47 <emmanuellutaaya4@gmail.com>
2023-06-15 13:53:45 -0700

    commitng_fifth

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - My first C program that prints a line with puts function
+ * Return: 0 (Passed)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
