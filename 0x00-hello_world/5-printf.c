9270bb7bf72bac3fac5a912b3140f2ff3beece29
Lutaaya47 <emmanuellutaaya4@gmail.com>
2023-06-17 09:59:26 -0700

    this is the fifth program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - a program with the ability to print a line using printf
+ * return: 0 (Worked)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n")
+		return (0);
+}
+
+
