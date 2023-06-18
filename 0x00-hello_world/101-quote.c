5c2d0cbbfde42243586f35ad5f22230bb905476a
Lutaaya47 <emmanuellutaaya4@gmail.com>
2023-06-18 03:22:45 -0700

    this has been my eigth c porgram

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 
 /**
  * main - A program that has the ability to print a line to the standard error
- * Return 1 (Worked)
+ * Return 1 (Worked and so there is an error)
  */
 int main(void)
 {
-	write(2 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return 1;
 }
