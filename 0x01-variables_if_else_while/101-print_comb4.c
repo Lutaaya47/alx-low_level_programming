10813e2da207552e6ed8d2871cb40100f18918e6
Lutaaya47 <emmanuellutaaya4@gmail.com>
2023-06-18 13:00:33 -0700

    sdhdsbsdhdsds

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,11 +8,11 @@ int main(void)
 {
 	int n, m, l;
 
-	for (n = 48; n > 58; n++)
+	for (n = 48; n < 58; n++)
 	{
-		for (m = 49; m < 58; m++)
+		for (m = 48; m < 58; m++)
 		{
-			for (l = 50; l < 58; l++)
+			for (l = 48; l < 58; l++)
 			{
 				if (l < m && m > n)
 				{
@@ -28,6 +28,6 @@ int main(void)
 			}
 		}
 	}
-	putchar('\n')
+	putchar('\n');
 	return (0);
 }
