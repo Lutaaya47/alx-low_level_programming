ad9084698deefbb4fe1fb18e1b32b372b34b2d72
Lutaaya47 <emmanuellutaaya4@gmail.com>
2023-06-18 03:10:44 -0700

    the eigth c program commit message

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * main - A program that has the ability to print a line to the standard error
  * Return 1 (Worked)
  */
-int main(viod)
+int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
