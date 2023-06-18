504ddd2f0e3dc2dbc3ef7a847c2adf80cf2c8f6c
Lutaaya47 <emmanuellutaaya4@gmail.com>
2023-06-18 09:26:21 -0700

    print all single digit numberas

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbres from 0 to 9
+ * Return: 0(Worked)
+ */
+int main(void)
+{
+	int i;
+	
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+	
