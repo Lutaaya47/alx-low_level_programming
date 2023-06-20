7995fb05970820e86955412572f48788bec0ca0b
Lutaaya47 <emmanuellutaaya4@gmail.com>
2023-06-20 07:47:02 -0700

    hfbhjf

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,11 @@
 #ifndef_main_h
 #define main_h
 
-int putchar(char);
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
-int _islpha(int c);
+int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
