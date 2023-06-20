d7b6705d1511ecb1c5329128c37c5533b15f1d04
Lutaaya47 <emmanuellutaaya4@gmail.com>
2023-06-20 07:59:08 -0700

    sghdvdshgs

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
-#ifndef_main_h
+#ifndef main_h
 #define main_h
+
 
 int _putchar(char);
 void print_alphabet(void);
@@ -16,3 +17,4 @@ void print_to_98(int n);
 void print_times_table(int n);
 
 #endif
+
